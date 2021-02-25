52e8b4926044c2fa35ff7b3c3309eff67df69742
JuliaKondratyuk <learner-ebbe6a@extensionschool.ch>
2021-02-25 15:49:33 +0100

    I have added first line of comments to my file

---
+++ converterTemp.py
@@ -0,0 +1,15 @@
+def converter(celsius):
+    msg_1 = " degrees Celsius are "
+    msg_2 = " degrees Farenheit."
+    farenheit = (celsius * 9/5) + 32
+    return str(celsius) + msg_1 + str(farenheit) + msg_2
+
+user_input = input( "Enter a themperature in Celsius: ")
+farenheit_result = converter(float(user_input))
+
+if (float(user_input)) > 38:
+    print("Its realllly hot!")
+
+print (farenheit_result)
+
+
